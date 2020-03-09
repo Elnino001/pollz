50a1cc15ddbfff2f49c83fc10fc8a8b2ec5a9105
Aniche Enare <henryenare001@gmail.com>
2020-03-09 15:57:53 +0100

    updated

--- polls/urls.py
+++ polls/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('pollsApp.urls', namespace='polls')),
+    path('', include('pollsApp.urls', namespace='polls')),
 ]
